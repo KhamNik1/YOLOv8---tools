cdbe78360e5a6c6ab72f0abff29247228661606a
KhamNik1 <147470728+KhamNik1@users.noreply.github.com>
2023-11-27 14:39:46 +0600

    Update checking the scales.py

--- checking the accuracy of the model/checking the scales.py
+++ checking the accuracy of the model/checking the scales.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("C:\\Nikita\\Desktop\\[]\\job\\RUNы\\DS1.7 yolon-100\\runs\\segment\\train\\weights\\last.pt")  # load a custom model
+model = YOLO("C:\\Desktop\\[]\\job\\RUNы\\DS1.7 yolon-100\\runs\\segment\\train\\weights\\last.pt")  # load a custom model
 
-source = "C:\\Users\\Desktop\\for pred\\online-audio-convert.com.mp4"
+source = "C:\\Desktop\\for pred\\online-audio-convert.com.mp4"
 # Run inference on the source
-model.predict(source="C:\\Nikita\\Desktop\\for pred\\online-audio-convert.com.mp4", save=True)  # predict on the video
+model.predict(source="C:\\Desktop\\for pred\\online-audio-convert.com.mp4", save=True)  # predict on the video
 
 results = model
