ccfac6c0c108791ac7b432023b8548fcb7ef97d3
KhamNik1 <147470728+KhamNik1@users.noreply.github.com>
2023-11-27 14:38:43 +0600

    Update neural network training on a prepared dataset.py

--- train/neural network training on a prepared dataset.py
+++ train/neural network training on a prepared dataset.py
@@ -5,5 +5,5 @@ import torchvision
 
 if __name__ == '__main__':
     model = YOLO('yolov8s.pt')
-    model.train(data="C:\\Users\\Nikita\\Desktop\\[]\\job\\dataset\\dataset_2.7\\data.yaml", epochs=60)   
-    results = model("C:\\Users\\Nikita\\Desktop\\для предикта\\2023-10-31_22-34-32.png", save=True)
+    model.train(data="C:\\Users\\Desktop\\[]\\job\\dataset\\dataset_2.7\\data.yaml", epochs=60)   
+    results = model("C:\\Users\\Desktop\\для предикта\\2023-10-31_22-34-32.png", save=True)
